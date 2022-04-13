5685c63ecefe1d5129191443d5e76bc09b4ef85f
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-13 12:10:36 -0300

    CodeIginter

---
+++ app/Controllers/Produtos.php
@@ -0,0 +1,21 @@
+<?php 
+
+  namespace App\Controllers;
+
+  use CodeIgniter\Controller;
+
+  class Produtos extends Controller{
+
+    public function index(){
+
+      return "Index Produtos";
+
+    }
+
+    public function novo(){
+
+      return "Novo Produto";
+
+    }
+
+  }
