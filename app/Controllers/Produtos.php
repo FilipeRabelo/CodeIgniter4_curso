41b4b33b89f6bb4a980d6836a318d41c808ad29f
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-14 18:53:02 -0300

    CodeIginter

--- app/Controllers/Produtos.php
+++ app/Controllers/Produtos.php
@@ -8,11 +8,31 @@
 
       public function index() {
 
-        $titulo = "Meus produtos";  // Vai para a view
+        $titulo = "Meus produtos";  // Vai para a view // 
 
+        $produtos = [               // ARRAY COM OS PRODUTOS // 
+          
+          [
+            "nome"            => "Notebook",
+            "descricao"       => "Produto usado",
+            "valor_de_compra" => 899.99,
+            "valor_de_venda"  => 1399.90,
+            "quantidade"      => 11,
+            "validade"        => ""
+          ],
 
+          [
+            "nome"            => "Computador I5",
+            "descricao"       => "Produto novo",
+            "valor_de_compra" => 1899.99,
+            "valor_de_venda"  => 2899.99,
+            "quantidade"      => 1,
+            "validade"        => ""
+          ],
 
-        $data["titulo"] = $titulo; // para mandar para o view// é preciso setar 
+        ];
+
+        $data["titulo"] = $titulo; // ARRAY para mandar para o view // é preciso setar 
 
         echo View("produtos/index", $data);  // ATRAVES DESSA LINHA DE COMANDO VAMOS ESTAR SETANDO A vIEW CRIADA EM PRODUTOS/INDEX
 
@@ -21,7 +41,9 @@
       public function novo() {
 
         return "Novo Produto";
+        
       }
+
     }
 
 ?>
