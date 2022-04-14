8eaa558f5f391e70f5546d920c118397827dcf99
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-14 14:17:53 -0300

    CodeIginter

--- app/Database/Seeds/MinhaPrimeiraSeed.php
+++ app/Database/Seeds/MinhaPrimeiraSeed.php
@@ -4,7 +4,7 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-#AQUI FICARA TODAS AS FUNÇÕES NATIVAS RESPONSAVEIS PELAS INSERÇÕES NO BD   
+#AQUI FICARA TODAS AS FUNÇÕES NATIVAS RESPONSAVEIS PELAS INSERÇÕES (insert) NO BD   
 class MinhaPrimeiraSeed extends Seeder
 {
     public function run()
@@ -13,10 +13,10 @@ class MinhaPrimeiraSeed extends Seeder
 
             'nome'            => 'Notebook',
             'descricao'       => 'Descrição do produto para nossa primeira Seed',
-            'valor_de_compra' => 1299, 90,
-            'valor_de_venda'  => 1599, 99,
+            'valor_de_compra' => 1299.90,
+            'valor_de_venda'  => 1599.99,
             'quantidade'      => 9,
-            'validade'        => '',
+            'validade'        => '2025-05-12',
 
         ]);
     }
