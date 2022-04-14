3300e1b01b825066f5d43dcda273bf84d3244943
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-14 12:34:47 -0300

    CodeIginter

--- app/Database/Migrations/2022-04-14-141157_Produtos.php
+++ app/Database/Migrations/2022-04-14-141157_Produtos.php
@@ -9,28 +9,32 @@ class Produtos extends Migration
 
    public function up()
    {      #METODO CRIADO QUE VAI SER CHAMADO PARA (CRIAR) A MIGRATION,
-      #TODA A PARTE DE CRIACAO DA NOSSA ESTRURTURA FO BD VAMOS COLCOAR AQUI DA TABELA PRODUTOS POR EX
+          #TODA A PARTE DE CRIACAO DA NOSSA ESTRURTURA FO BD VAMOS COLCOAR AQUI DA TABELA PRODUTOS POR EX
 
-      $this->forge->addField([            # CRIANDO AS TABELAS #
+
+      // INICIO ESTRUTURA DA TABELA COM COLUNAS
+
+      // PARA CRIAR AS TABELAS addField() -> php spark migrates
+      $this->forge->addField([          
 
          'id_produto'        => [
             'type'           => "INT",
-            'contraint'      => 9,   # CONTRAINT -> TAMANHO DO NUMERO
+            'contraint'      => 9,      # CONTRAINT -> TAMANHO DO NUMERO
             'usigned'        => true,   # USADO PARA INCREMENTAR
             'auto_increment' => true,
          ],
 
          'nome'          => [
             'type'       => 'VARCHAR',
-            'constraint' => 128     # Caractere maximos = no curso de banco de dados 
+            'constraint' => 128         # Caractere maximos = no curso de banco de dados 
          ],
 
          'descricao'  => [
-            'type'    => 'TEXT'    # NAO PRECISA DE CONSTRAINT POIS NAO HA LIMITE DE CARACTERE AQUI
+            'type'    => 'TEXT'         # NAO PRECISA DE CONSTRAINT POIS NAO HA LIMITE DE CARACTERE AQUI
          ],
 
          'valor_de_compra' => [
-            'type'         => 'DOUBLE'  # DOUBLEPQ VAI SER UM VALOR
+            'type'         => 'DOUBLE'  # DOUBLE PQ VAI SER UM VALOR
          ],
 
          'valor_de_venda' => [
@@ -41,18 +45,38 @@ class Produtos extends Migration
             'type'    => "INT"
          ],
 
-         'validade'   => [   #CASO ESSE PRODUTO TENHA UMA VALIDADE 
-            'type'    => 'DATE'
-         ]
-
-
+         'validade'   => [   
+            'type'    => 'DATE'         #CASO ESSE PRODUTO TENHA UMA VALIDADE 
+         ],
+         
+         'created_at' => [
+            'type'    => "DATETIME"   
+         ],
+         
+         'updated' => [
+            'type'    => "DATETIME"       # ----> SERVE PARA REGISTAR TODA VEZ Q ALTERAR OU EXCLUIR OU CRIAR ALGO NO BANCO DE DADOS
+         ],                               # DATA HORA DIA ETC...
+         
+         'delete' => [
+            'type'    => "DATETIME"   
+         ],         
+         
       ]);
+
+      $this->forge->addKey('id_produto', true);  #INDICANDO QUAL VAI SER A CHAVE DA MINHA TABELA e TRUE PARA FALAR Q ESTA ATIVO
+      $this->forge->createTable('produtos');     # CODIGO PARA CRIAR REALMENTE A TABELA
+
+      #TODAS VEZ Q EU RPDAR ESSA MIGRATE AUTOMATICAMENTRE ELE VAI VIR E CRIAR A ESTRUTURA DESSA MINAH TABELA
+
+   }  // FIM DA CRIACAO DA TABELA
+
+
+   //  PARA APAGAR (remover) A TABELA up(); php spark migrates:rollback
+
+   public function down() {   # QUANDO QUISER APAGAR ESSA TABELA AUTOMATICAMENTE EU VOU COLOCOAR A ESTRUTURA AQUI DENTRO
+
+      $this->forge->dropTable('produtos');
+
    }
-
-   public function down()
-   {   # QUANDO QUISER APAGAR ESSA TABELA AUTOMATICAMENTE EU VOU COLCOAR A ESTRUTURA AQUI DENTRO
-
-
-      //
-   }
+   
 }
