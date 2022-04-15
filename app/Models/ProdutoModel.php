55f6398ea38879de5af1d5a8bef42dedff560b56
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-14 22:15:00 -0300

    CodeIginter

--- app/Models/ProdutoModel.php
+++ app/Models/ProdutoModel.php
@@ -1,17 +1,25 @@
+<!-- //* MODEL -> Seria a parte da modelagem de dados e regras de negócio.
+// É nela que vão constar as classes ,
+// as consultas ao banco de dados e as regras de negócio do sistema.
+// TERA MAIS METODOS AQUI NESTE MODEL
+// VAMOS MOSTRAR O BANCO DE DADOS
+// ESTE MODULO VAI INSERIR OS REGISTROS E TERA PERMISAO NO NOSSO BANCO DE DADOS -->
+
+
 <?php
 
-  namespace App\Controllers;
+namespace App\Controllers;
 
-  use CodeIgniter\Model;
+use CodeIgniter\Model;
 
-  class ProdutoModel extends Model{
+class ProdutoModel extends Model
+{
 
-    public function cadastraRegistro(){  // METODO PARA SER UTILIZADO FUTURAMENTE NO PROGRAMA
+  public function cadastraRegistro()
+  {  // METODO PARA SER UTILIZADO FUTURAMENTE NO PROGRAMA
 
-      return "Modulo responsavel por cadastrar todos os registros do produto ";
-
-    }
-
+    return "Modulo responsavel por cadastrar todos os registros do produto ";
   }
+}
 
 ?>
