16b8d1a38d9a114579182d99ee2d2ed23a8e6442
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-14 22:14:17 -0300

    CodeIginter

--- app/Views/produtos/index.php
+++ app/Views/produtos/index.php
@@ -1,23 +1,28 @@
+<!-- ESTA É A VIEW -->
 
-                  <!-- ESTA É A VIEW -->
+<!-- VIEW -> É a parte estética, a interface. 
+É a "tela" onde os dados e os links serão apresentados ao usuário.
+De grosso modo, um arquivo somente com códigos HTML e CSS 
+(e se tiver, o Javascript também).  -->
 
 <!DOCTYPE html>
 <html lang="pt-br">
 
-  <head>
+<head>
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-   
-  </head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 
-  <body>
+</head>
 
-   <h1><?= $titulo  ?></h1>    
-   
+<body>
 
-  </body>
+  <h1><?= $titulo  ?></h1>
+  <h1><?= $nome    ?></h1>
+  <h1><?= var_dump($produtos) ?></h1>
+
+</body>
 
 </html>
